eaba9de45f8da34fc914cd50a6f033f719c584aa
Olyzen14 <44410560+Olyzen14@users.noreply.github.com>
2018-10-31 17:33:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ if (message.content === "Comment tu vas Codis ?"){
 function random(min, max) {
     min = Math.ceil(0);
     max = Math.floor(3);
-    randum = Math.floor(Math.random() * (max - min +1) + min);
+    randnum = Math.floor(Math.random() * (max - min +1) + min);
 }
     
 bot.login(process.env.TOKEN);
